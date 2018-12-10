85b6248d4f6f03717285aefa0157ecea152cbc5d
Harrison <38505228+katznboyz1@users.noreply.github.com>
2018-12-10 14:50:55 -0500

    Add files via upload

---
+++ i.py
@@ -0,0 +1,11 @@
+i = print
+ii = str
+iii = int
+iiii = float
+iiiii = range
+iiiiii = 10
+iiiiiii = 0
+iiiiiiii = exit
+for iiiiii in iiiii(iii(iiiiiii), iii(iiiiii)):
+    i(ii(iiiiii))
+iiiiiiii('i')
